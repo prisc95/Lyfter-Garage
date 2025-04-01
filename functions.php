c9cc37bd5626ff837a455c723bff94cc721a9684
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-04-01 09:44:18 +0200

    Added ACF to functions.php

--- functions.php
+++ functions.php
@@ -63,3 +63,30 @@ collect(['setup', 'filters'])
             );
         }
     });
+
+
+/*
+|--------------------------------------------------------------------------
+| Register ACF
+|--------------------------------------------------------------------------
+
+
+    /**
+     * We use WordPress's init hook to make sure
+     * our blocks are registered early in the loading
+     * process.
+     *
+     * @link https://developer.wordpress.org/reference/hooks/init/
+     */
+    function sage_register_acf_blocks() {
+        /**
+         * We register our block's with WordPress's handy
+         * register_block_type();
+         *
+         * @link https://developer.wordpress.org/reference/functions/register_block_type/
+         */
+        register_block_type( __DIR__ . '/blocks/test' );
+    }
+    
+    // Here we call our tt3child_register_acf_block() function on init.
+    add_action( 'init', 'sage_register_acf_blocks' );
